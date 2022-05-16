20c4efbb2fbe0115d3f21bd48236417c24d76222
Oskar <oskarpasko@gmail.com>
2022-05-16 10:37:19 +0200

    Zadanie 1 z dnia 16.05.2022

--- 16052022/src/com/company/Main.java
+++ 16052022/src/com/company/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Statek statek = new Statek();
+        statek.setRodzaj("Statek pasażerski");
+        statek.Plyn();
+
+        Samolot plane = new Samolot();
+        plane.setRodzaj("Bombowiec");
+        plane.Lec();
+
     }
 }
