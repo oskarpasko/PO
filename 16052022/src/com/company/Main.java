abd147d701fd872a2cf8906b5847dada59e7afe1
Oskar <oskarpasko@gmail.com>
2022-05-16 10:55:30 +0200

    Zadanie 2 z dnia 16.05.2022

--- 16052022/src/com/company/Main.java
+++ 16052022/src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+        System.out.println("Zadanie 1 ----- ");
         Statek statek = new Statek();
         statek.setRodzaj("Statek pasażerski");
         statek.Plyn();
@@ -13,5 +14,13 @@ public class Main {
         plane.setRodzaj("Bombowiec");
         plane.Lec();
 
+        System.out.println("Zadanie 2 ----- ");
+        Wieloryb rybka = new Wieloryb();
+        rybka.setGatunek("Wieloryb oceaniczny");
+        rybka.jedz();
+        rybka.Plyn();
+        rybka.wydalaj();
+        rybka.Wynurz();
+        rybka.Zanurz();
     }
 }
